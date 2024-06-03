b16bcb0624bc0746a1b0e249f992cb5fb869fde8
PaulaMaina <kabura.mainap@gmail.com>
2024-06-03 10:06:00 -0700

    Fetch places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,61 @@
+const $ = window.$;
+
+$('document').ready(init);
+
+function init () {
+  const amenityObject = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObject[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityObject[$(this).attr('data-name')];
+    }
+    const names = Object.keys(amenityObject);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+  apiStatus();
+  getPlaces();
+}
+
+function apiStatus () {
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function (data, textStat) {
+    if (textStat === 'success' && data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('api_status').removeClass('available');
+    }
+  });
+}
+
+function getPlaces () {
+  const placesUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+  $.ajax({
+    url: placesUrl,
+    type: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    data: JSON.stringify({}),
+    success: function (response) {
+      for (const res of response) {
+        const article = ['<article>',
+          '<div class="title_box">',
+          `<h2>${res.name}</h2>`,
+          `<div class="price_by_night">$${res.price_by_night}</div>`,
+          '</div>',
+          '<div class="information">',
+          `<div class="max_guest">${res.max_guest} Guest(s)</div>`,
+          `<div class="number_rooms">${res.number_rooms} Bedroom(s)</div>`,
+          `<div class="number_bathrooms">${res.number_bathrooms} Bathroom(s)</div>`,
+          '</div>',
+          '<div class="description">',
+          `${res.description}`,
+          '</div>',
+          '<article>'];
+        $('SECTION.places').append(article.join(''));
+      }
+    },
+    error: (err) => {
+      console.log(err);
+    }
+  });
+}
